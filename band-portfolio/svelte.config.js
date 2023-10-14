c5773c610190a249acf110a8c3f501555f9fc15e
Tamassp <petertamas05@gmail.com>
2023-10-14 15:33:58 +0200

    Deploy update

--- band-portfolio/svelte.config.js
+++ band-portfolio/svelte.config.js
@@ -6,19 +6,33 @@ import preprocess from 'svelte-preprocess';
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
 
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	//preprocess: preprocess(),
 	preprocess: [vitePreprocess()],
+	trailingSlash: 'always',
 	kit: {
-		adapter: adapter(),
-		path: {
-			base: dev ? '' : process.env.BASE_PATH || '/band-portfolio'
-		}
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		})
 	}
+	// kit: {
+	// 	adapter: adapter(),
+		
+	// 	// path: {
+	// 	// 	base: dev ? '' : process.env.BASE_PATH || '/band-portfolio'
+	// 	// }
+	// 	// path: {
+	// 	// 	base: '/band-portfolio'
+	// 	// }
+	// }
 	//preprocess: vitePreprocess()
 };
 
