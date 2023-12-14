0f311bf51e5a26dedbb408c63ef637be50f0cbe9
Tamassp <petertamas05@gmail.com>
2023-12-14 10:02:11 +0100

    Add disabled button for Bands Portfolio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -118,14 +118,10 @@
 
 			<h3 class="">Software</h3>
 		</LinkButton>
-		<!-- <LinkButton
-			id="bands"
-			on:mouseenter={handleMouseEnter}
-			on:mouseleave={handleMouseLeave}
-			navigateTo="/bands"
+		<button disabled id = "buttonTempStyle"
 		>
 			<svg
-				id="note"
+				id="/*note"
 				xmlns="http://www.w3.org/2000/svg"
 				fill="none"
 				viewBox="0 0 24 24"
@@ -139,8 +135,8 @@
 					d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z"
 				/>
 			</svg>
-			Bands
-		</LinkButton> -->
+			Bands (soon)
+		</button>
 	</nav>
 </div>
 
@@ -153,6 +149,24 @@
 		color: white; */
 		display: flex;
 		justify-content: center;
+	}
+	#buttonTempStyle {
+		background-color: rgb(126, 151, 156);
+		color: white;
+		margin: 8px;
+		padding: 14px 25px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		height: 128px;
+		width: 128px;
+
+		/* border: 4px solid red; */
+		border-radius: 4px;
+		transition:  all 0.5s ease;
+	}
+	#bands {
+		background-color: gray;
 	}
 
 	#upper {
